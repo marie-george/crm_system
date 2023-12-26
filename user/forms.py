e4a049dd09ded2ed32fa44df16d21fa86197bcbe
marie-george <maria85night@gmail.com>
2023-12-27 02:04:27 +0300

    subsequent updates

---
+++ user/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import User
+
+
+class UserRegisterForm(UserCreationForm):
+
+    username = forms.CharField(
+        label='логин',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'autocomplete': 'off'})
+    )
+    password1 = forms.CharField(
+        label='пароль',
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    password2 = forms.CharField(
+        label='подтверждение пароля',
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    email = forms.EmailField(
+        label='почта',
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
